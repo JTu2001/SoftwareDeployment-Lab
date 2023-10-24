a2ff049f5d8b9e0880e5cf81556bdba99565c297
TUNJIC Josip <josip.tunjic77@gmail.com>
2023-10-24 16:50:06 +0200

    Created Simple App

---
+++ Lab2/nodeJS-smart-home/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/getVoltage', (req, res) => {
+    res.send('3,3V');
+});
+
+app.get('/getTemperature', (req, res) => {
+    res.send('28,5°C');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
